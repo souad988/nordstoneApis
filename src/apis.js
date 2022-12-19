da378838f3e92eaa0f71ecfd22ff24bc34c7063b
souad988 <59707859+souad988@users.noreply.github.com>
2022-12-19 19:47:17 +0100

    Add redirection to netlifyfile

--- src/apis.js
+++ src/apis.js
@@ -42,7 +42,7 @@ router.get("/multiply", (req, res) => {
 //   console.log("nordstone app is listening on port", port);
 // });
 
-app.use("/.netlify/functions/apis", router);
+app.use("/", router);
 module.exports.handler = serverless(app);
 
 // [[redirects]];
